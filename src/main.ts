4c88105dd2442947f9d1b6a26a889068e76e5e4c
Gabriel Chagas <gabrielserq@outlook.com>
2023-07-08 22:19:05 -0300

    chore: creating main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -10,3 +12,5 @@ import { ValidationPipe } from '@nestjs/common';
     }),
   );
   await app.listen(3000);
+}
+bootstrap();
