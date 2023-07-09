40c16f977f9e5f34f5345b6af9fdd1a9d8b2608e
Gabriel Chagas <gabrielserq@outlook.com>
2023-07-08 22:18:39 -0300

    chore: creating useGlobalPipes settings

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
