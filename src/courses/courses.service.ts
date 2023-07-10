27a7e51e0c2b1068d855fb7739b1e63694bb8e14
Gabriel Chagas <gabrielserq@outlook.com>
2023-07-09 22:23:14 -0300

    chore: creating array courses

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,3 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { Course } from './entities/course.entity';
 
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: 'Fundamentos do framework NestJS',
+      description: 'Fundamentos do framework NestJS',
+      tags: ['node.js', 'nestjs', 'javascript'],
