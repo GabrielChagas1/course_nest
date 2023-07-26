c63d0f95a197ab2e3f862554ec2aafe62dd31cf1
Gabriel Chagas <gabrielserq@outlook.com>
2023-07-25 22:25:07 -0300

    import entities injectable and notfoundexception

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Course } from './entities/course.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
