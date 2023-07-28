95a6c9f980ed2226ddc98cbcf50a86d880ba566d
Gabriel Chagas <gabrielserq@outlook.com>
2023-07-27 22:39:03 -0300

    creating and exporting class Tag

--- src/courses/entities/tag.entity.ts
+++ src/courses/entities/tag.entity.ts
@@ -6,11 +6,15 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { Course } from './course.entity';
+
 @Entity('tags')
+export class Tag {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
   name: string;
+
   @ManyToMany(() => Course, (course) => course.tags)
   courses: Course[];
+}
