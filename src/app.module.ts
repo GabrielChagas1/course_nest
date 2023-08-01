66ba0ebff5e152b8530bd3e4335b38ff9724a603
Gabriel Chagas <gabrielserq@outlook.com>
2023-08-01 13:29:21 -0300

    refactoring connection for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     CoursesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: 'postgres',
+    //   database: 'postgres',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
