b552234a90c73437a3ab1cb24f1564c8535f8812
Gabriel Chagas <gabrielserq@outlook.com>
2023-08-02 13:33:29 -0300

    create provide 'DATA_SOURCE'

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -1,3 +1,5 @@
 import { DataSource } from 'typeorm';
 import { CourseRefactoringTest1691443166263 } from './migrations/1691443166263-CourseRefactoringTest';
 export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
