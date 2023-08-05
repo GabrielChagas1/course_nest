64f0672d078fabf37736beef5dac843456895d11
Gabriel Chagas <gabrielserq@outlook.com>
2023-08-05 13:40:19 -0300

    import coursesProviders

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
 import { DatabaseModule } from 'src/database/database.module';
+import { coursesProviders } from './courses.providers';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Course, Tag])],
