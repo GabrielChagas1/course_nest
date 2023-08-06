ebcc386a0afe04e5dd0790ca40bdad1a4efb13ee
Gabriel Chagas <gabrielserq@outlook.com>
2023-08-06 13:44:18 -0300

    create inject first providers

--- src/courses/courses.providers.ts
+++ src/courses/courses.providers.ts
@@ -5,3 +5,4 @@ import { Tag } from './entities/tag.entity';
 export const coursesProviders = [
     provide: 'COURSES_REPOSITORY',
     useFactory: (dataSource: DataSource) => dataSource.getRepository(Course),
+    inject: ['DATA_SOURCE'],
