fb81df314b6a033850579d4c83a8e46211d3d64d
Gabriel Chagas <gabrielserq@outlook.com>
2023-08-06 13:44:54 -0300

    finishe first provider

--- src/courses/courses.providers.ts
+++ src/courses/courses.providers.ts
@@ -3,6 +3,8 @@ import { Course } from './entities/course.entity';
 import { Tag } from './entities/tag.entity';
 
 export const coursesProviders = [
+  {
     provide: 'COURSES_REPOSITORY',
     useFactory: (dataSource: DataSource) => dataSource.getRepository(Course),
     inject: ['DATA_SOURCE'],
+  },
