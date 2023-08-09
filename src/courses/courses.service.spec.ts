36f3fedb593b9a2ced3f2e10c2f57a7716de15e4
Gabriel Chagas <gabrielserq@outlook.com>
2023-08-09 13:58:46 -0300

    creating expectOutputCourse

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -71,6 +71,15 @@ describe('CoursesService', () => {
         created_at: date,
       },
     ];
+    const expectOutputCourse = [
+      {
+        id,
+        name: 'Test',
+        description: 'Test description',
+        created_at: date,
+        tags: expectOutputTags,
+      },
+    ];
 
         courseRepository.findOne.mockReturnValue(expectedCourse);
         const course = await service.findOne(courseId);
