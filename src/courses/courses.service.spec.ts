d20ffde9f9010af5d27e8eebe23676d790a29bf6
Gabriel Chagas <gabrielserq@outlook.com>
2023-08-15 14:07:55 -0300

    creating expectOutputCourse

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -133,6 +133,15 @@ describe('CoursesService', () => {
         created_at: date,
       },
     ];
+    const expectOutputCourse = [
+      {
+        id,
+        name: 'Test',
+        description: 'Test description',
+        created_at: date,
+        tags: expectOutputTags,
+      },
+    ];
     });
   });
 });
