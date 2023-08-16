424f29e662f7588f0d3afc094c95e732b36a7e14
Gabriel Chagas <gabrielserq@outlook.com>
2023-08-16 14:19:35 -0300

    import components for nestjs

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Inject, NotFoundException } from '@nestjs/common';
 import { Course } from './entities/course.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
